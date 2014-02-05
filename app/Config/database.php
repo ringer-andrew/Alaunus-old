90cc9dc2d76ecc9f452c00ac4725cd3f54e913a4
Andrew Ringer <andrew.ringer@professionalperformance.ca>
2014-02-05 03:10:36 -0500

    eb database config

--- app/Config/database.php
+++ app/Config/database.php
@@ -59,27 +59,35 @@
  * For Postgres : http://www.postgresql.org/docs/9.2/static/sql-set.html
  * For Sql Server : http://msdn.microsoft.com/en-us/library/ms190356.aspx
  */
+
+if (!defined('RDS_HOSTNAME')) {
+	define('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']);
+	define('RDS_USERNAME', $_SERVER['RDS_USERNAME']);
+	define('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']);
+	define('RDS_DB_NAME', $_SERVER['RDS_DB_NAME']);
+}
+
 class DATABASE_CONFIG {
 
 	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'database_name',
-		'prefix' => '',
+	    'datasource' => 'Database/Mysql',
+	    'persistent' => false,
+	    'host' => RDS_HOSTNAME,
+	    'login' => RDS_USERNAME,
+	    'password' => RDS_PASSWORD,
+	    'database' => RDS_DB_NAME,
+	    'prefix' => '',
 		//'encoding' => 'utf8',
-	);
-
+  	);
+					
 	public $test = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'user',
-		'password' => 'password',
-		'database' => 'test_database_name',
-		'prefix' => '',
-		//'encoding' => 'utf8',
+	    'datasource' => 'Database/Mysql',
+	    'persistent' => false,
+	    'host' => 'localhost',
+	    'login' => 'user',
+	    'password' => 'password',
+	    'database' => 'test_database_name',
+	    'prefix' => '',
+	    //'encoding' => 'utf8',
 	);
 }
